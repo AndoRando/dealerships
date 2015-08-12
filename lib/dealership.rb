b90b3d916074e3e517b71a0a1dbc729560c4d290
Andrew Brennwald & Austin Kincaid <student@epicodus.com>
2015-08-12 11:34:44 -0700

    Add spec and method to return dealership ID

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,9 +1,16 @@
 class Dealership
+  @@dealerships = []
+
   define_method(:initialize) do |name|
     @d_name = name
+    @d_id = @@dealerships.length().+(1)
   end
 
   define_method(:d_name) do
     @d_name
+  end
+
+  define_method(:d_id) do
+    @d_id
   end
 end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -5,8 +5,15 @@ require('vehicle')
 describe(Dealership) do
   describe('#d_name') do
     it('returns the name of this particular dealershack') do
-      test_dealership = Dealership.new("Chuck's VW Shack")
-      expect(test_dealership.d_name()).to(eq("Chuck's VW Shack"))
+      austins_dealership = Dealership.new("Austin-and-out Auto Magisterium")
+      expect(austins_dealership.d_name()).to(eq("Austin-and-out Auto Magisterium"))
+    end
+  end
+
+  describe('#d_id') do
+    it('returns unique dealerchip ID') do
+      brennwalds_dealership = Dealership.new("Brennwald's Wall-to-Wall Auto-Hall")
+      expect(brennwalds_dealership.d_id()).to(eq(1))
     end
   end
 end
